126e57db3f5fdfe556f962cff467b56c56252571
dedislamets <dedi.supatman@modena.co.id>
2021-03-31 01:00:00 +0700

    site fix

--- application/views/sitemap/index.php
+++ application/views/sitemap/index.php
@@ -2,29 +2,26 @@
   header('Content-type: application/xml; charset="ISO-8859-1"',true);  
 ?>
  
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <url>
     <loc><?= base_url() ?></loc>
-    <lastmod><?= date('d-m-Y H:i:s') ?></lastmod>
-    <changefreq>daily</changefreq>
+    <lastmod>2021-03-30T17:54:58+00:00</lastmod>
     <priority>0.1</priority>
   </url>
   <url>
     <loc><?= base_url('about') ?></loc>
-    <lastmod>30-03-2021 12:00:00</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>0.1</priority>
+    <lastmod>2021-03-30T17:54:58+00:00</lastmod>
+    <priority>0.80</priority>
   </url>
   <url>
     <loc><?= base_url('services') ?></loc>
-    <lastmod>31-03-2021 12:40:00</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>0.1</priority>
+    <lastmod>2021-03-30T17:54:58+00:00</lastmod>
+    <priority>0.80</priority>
   </url>
   <url>
     <loc><?= base_url('contact') ?></loc>
-    <lastmod>31-03-2021 12:42:00</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>0.1</priority>
+    <lastmod>2021-03-30T17:54:58+00:00</lastmod>
+    <priority>0.80</priority>
   </url>
 </urlset>
