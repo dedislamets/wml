1e3e4f20afb14e37772b769b2b691b4eefe76a91
dedislamets <dedi.slamets@gmail.com>
2024-02-23 00:33:39 +0700

    remove amchart

--- application/views/home/js.php
+++ application/views/home/js.php
@@ -6,96 +6,10 @@
 
 <script type="text/javascript">
   $(document).ready(function(){ 
-    //Combo chart
-    google.charts.load('current', { 'packages': ['corechart'] });
-    google.charts.setOnLoadCallback(drawVisualization);
 
-    function drawVisualization() {
-        // Some raw data (not necessarily accurate)
-        var data = google.visualization.arrayToDataTable([
-            ['Month', 'Vendor', 'Customer'],
-            ['2004/05', 165, 938],
-            ['2005/06', 135, 1120],
-            ['2006/07', 157, 1167],
-            ['2007/08', 139, 1110],
-            ['2008/09', 136, 691]
-        ]);
-
-        var options = {
-            vAxis: { title: 'Payment' },
-            hAxis: { title: 'Month' },
-            seriesType: 'bars',
-            // series: { 2: { type: 'line' } },
-            colors: ['#93BE52', '#69CEC6', '#FE8A7D']
-        };
-
-        var chart = new google.visualization.ComboChart(document.getElementById('chart_Combo'));
-        chart.draw(data, options);
-    }
-
-
-
-
-    AmCharts.makeChart(
-    "proj-earning",
-    {
-      type:"serial",
-      hideCredits:!0,
-      theme:"light",
-      dataProvider:[
-        {
-          type:"UI",visits:10
-        },
-        {
-          type:"UX",visits:15
-        },
-        {
-          type:"Web",visits:12
-        },
-        {
-          type:"App",visits:16
-        },
-        {
-          type:"SEO",visits:8
-        }],
-      valueAxes:[
-        {
-          gridAlpha:.3,
-          gridColor:"#fff",
-          axisColor:"transparent",
-          color:"#fff",dashLength:0
-        }]
-      ,gridAboveGraphs:!0,
-      startDuration:1,
-      graphs:[
-        {
-          balloonText:"Active User: <b>[[value]]</b>",
-          fillAlphas:1,
-          lineAlpha:1,
-          lineColor:"#fff",
-          type:"column",
-          valueField:"visits",
-          columnWidth:.5
-        }],
-      chartCursor:{
-        categoryBalloonEnabled:!1,
-        cursorAlpha:0,
-        zoomable:!1
-      },
-      categoryField:"type",
-      categoryAxis:{
-        gridPosition:"start",
-        gridAlpha:0,
-        axesAlpha:0,
-        lineAlpha:0,
-        fontSize:12,
-        color:"#fff",
-        tickLength:0},
-      export:{enabled:!1}
-    });
   });
   
-  var app = new Vue({
+  /*var app = new Vue({
       el: "#app",
       mounted: function () {
         this.loadHistory();
@@ -176,7 +90,7 @@
         },
         
       }
-  })
+  })*/
 
   function initialize(){
 
